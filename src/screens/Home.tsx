2be61b9ed399197fc484c531edbc738e630941fc
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-12-19 17:47:18 -0300

    feat: save select image

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,21 @@
-import { Text } from 'native-base'
+import { useAuth } from '@hooks/auth.hook'
+import { api } from '@services/api'
+import { Avatar, Center, Image, Text } from 'native-base'
+import { User } from 'phosphor-react-native'
 import { type ReactNode } from 'react'
 
 export function Home (): ReactNode {
+  const { user } = useAuth()
   return (
-    <Text>Home</Text>
+    <Center flex={1}>
+      <Avatar borderColor="blue.500" borderWidth={4} size={24}>
+            {
+              user?.avatar
+                ? <Image source={{ uri: `${api.defaults.baseURL}/images/${user?.avatar}` }} alt='foto de perfil' />
+                : <User size={64} color="#5F5B62" />
+            }
+          </Avatar>
+      <Text>Home</Text>
+    </Center>
   )
 }
--- src/screens/auth/Signup.tsx
+++ src/screens/auth/Signup.tsx
@@ -3,13 +3,22 @@ import { Input } from '@components/Input'
 import { Logo } from '@components/Logo'
 import { type SignupDTO } from '@dtos/signup.dto'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { useAuth } from '@hooks/auth.hook'
 import { useNavigation } from '@react-navigation/native'
 import { type AuthNavigatorRoutesprops } from '@routes/auth.routes'
-import { Avatar, Center, Heading, IconButton, ScrollView, Text, VStack } from 'native-base'
+import { api } from '@services/api'
+import { AppError } from '@utils/errors/app.error'
+import * as FileSystem from 'expo-file-system'
+import * as ImagePicker from 'expo-image-picker'
+import { Avatar, Center, Heading, IconButton, Image, ScrollView, Text, VStack, useToast } from 'native-base'
 import { PencilSimpleLine, User } from 'phosphor-react-native'
 import { useState, type ReactElement } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import * as Yup from 'yup'
+
+type PhotoInfo = FileSystem.FileInfo & {
+  size: number
+}
 
 const signupValidationSchema = Yup.object({
   name: Yup.string().required('Insira o seu nome'),
@@ -29,13 +38,71 @@ export function Signup (): ReactElement {
     resolver: yupResolver(signupValidationSchema)
   })
 
-  const [isLoading, setIsLoading] = useState(false)
+  const { signup, signin, isLoading } = useAuth()
+  const toast = useToast()
 
-  const onSubmit = (data: SignupDTO): void => {
-    setIsLoading(true)
-    setTimeout(() => {
-      setIsLoading(false)
-      console.log(data)
+  const [formData, setFormData] = useState<FormData>({} as FormData)
+  const [avatar, setAvatar] = useState<string>()
+
+  const handleSelectImage = async (): Promise<void> => {
+    try {
+      const selectedImage = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true
+      })
+
+      if (selectedImage.canceled) {
+        // eslint-disable-next-line no-useless-return
+        return
+      }
+
+      if (selectedImage.assets?.[0]?.uri) {
+        const photoInfo = await FileSystem.getInfoAsync(selectedImage.assets[0].uri, { size: true }) as PhotoInfo
+
+        if (photoInfo?.size && (photoInfo.size / 1024 / 1024 > 3)) {
+          return toast.show({
+            title: "'Imagem muito grande. Por favor, escolha uma de até 3MB'",
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+        }
+
+        const fileExtension = selectedImage.assets[0].uri.split('.').pop()
+
+        const imageFile = {
+          name: `userAvatar.${fileExtension}`.toLowerCase(),
+          uri: selectedImage.assets[0].uri,
+          type: `${selectedImage.assets[0].type}/${fileExtension}`
+        } as any
+
+        const uploadForm = new FormData()
+        uploadForm.append('avatar', imageFile)
+
+        setAvatar(imageFile.uri)
+        setFormData(uploadForm)
+      }
+    } catch (error) {
+
+    }
+  }
+
+  const onSubmit = async (signupDTO: SignupDTO): Promise<void> => {
+    try {
+      formData.append('name', signupDTO.name)
+      formData.append('email', signupDTO.email)
+      formData.append('password', signupDTO.password)
+      formData.append('tel', signupDTO.phone)
+      await signup(formData)
+      await signin({ email: signupDTO.email, password: signupDTO.password })
+    } catch (error) {
+      toast.show({
+        title: error instanceof AppError ? error.message : 'Algo deu errado',
+        placement: 'top',
+        color: 'red.500'
+      })
+    } finally {
       reset({
         name: '',
         email: '',
@@ -43,7 +110,7 @@ export function Signup (): ReactElement {
         password: '',
         confirmPassword: ''
       }, { keepErrors: false })
-    }, 3000)
+    }
   }
 
   function handleGoToSignin (): void {
@@ -86,7 +153,11 @@ export function Signup (): ReactElement {
 
         <Center mt={4}>
           <Avatar borderColor="blue.500" borderWidth={4} size={24}>
-            <User size={64} color="#5F5B62" />
+            {
+              avatar
+                ? <Image source={{ uri: `${api.defaults.baseURL}/images/${avatar}` }} alt='foto de perfil' />
+                : <User size={64} color="#5F5B62" />
+            }
           </Avatar>
           <IconButton
             _pressed={{ backgroundColor: 'blue.500' }}
@@ -99,6 +170,7 @@ export function Signup (): ReactElement {
             rounded="full"
             size={10}
             icon={<PencilSimpleLine color='white' size={20} />}
+            onPress={handleSelectImage}
           />
         </Center>
 
